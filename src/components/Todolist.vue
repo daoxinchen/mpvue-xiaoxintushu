dde0c13b001d3356ed3429f7cdddeee4762d094e
dxchen <253743952@qq.com>
2019-10-29 23:35:17 +0800

    Initial Commit

---
+++ src/components/Todolist.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        todulist
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/pages/todolist/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>vuejs is good</h1>
+        总有一些不如人意的地方
+        <!-- <TodoList></TodoList> -->
+    </div>
+</template>
+
+<script>
+import TodoList from '@/components/Todolist'
+export default {
+  componetns: {
+    TodoList
+  }
+}
+</script>
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
